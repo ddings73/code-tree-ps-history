99b2af12ee72cc63276159a6e93272266188eade
ddings <ddings7303@gmail.com>
2024-11-29 12:26:50 +0900

    Title: 코드트리 등산 게임, Time: 1057ms, Memory: 73MB, Status: Passed - CodeTree

--- 241129/코드트리 등산 게임/codetree-mountain-climbing-games.java
+++ 241129/코드트리 등산 게임/codetree-mountain-climbing-games.java
@@ -3,11 +3,23 @@ import java.io.*;
 
 public class Main {
     private static int Q, N;
-    private static ArrayDeque<Integer> dq = new ArrayDeque<>();
+    private static List<Integer> stack = new ArrayList<>();
+    private static List<ArrayDeque<Integer>> mountain = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Q = Integer.parseInt(br.readLine());
+
+        /*
+            산 랜덤 선택
+            오른쪽 이동하면 100만점
+            케이블카 타면 100만점
+            더이상 이동 못하면 해당 산 높이만큼 점수 추가
+
+            첫 명령과 마지막 명령을 제외한 모든 명령이 산의 추가라면 약 550000의 산 개수가 됨
+            오른쪽 이동이 반드시 바로 옆 산으로 가는 것이 아님
+            오른쪽에 있는 산 중에서 내 위치보다 높기만 하면 되는듯
+        */
 
         StringBuilder sb = new StringBuilder();
         while(Q-- > 0){
@@ -17,71 +29,33 @@ public class Main {
             if(command == 100){ // 빅뱅
                 N = Integer.parseInt(stk.nextToken());
                 for(int i = 0; i < N; i++){
-                    dq.addLast(Integer.parseInt(stk.nextToken()));
+                    int height = Integer.parseInt(stk.nextToken());
+                    int idx = mountain.size() == 0 ? 0 : binarySearch(height);
+                    stack.add(idx);
+                    if(idx == mountain.size()) mountain.add(new ArrayDeque<>());
+                    mountain.get(idx).addLast(height);
                 }
-                
             }else if(command == 200){ // 우공이산
                 int height = Integer.parseInt(stk.nextToken());
-
-                dq.addLast(height);
+                int idx = mountain.size() == 0 ? 0 : binarySearch(height);
+                stack.add(idx);
+                if(idx == mountain.size()) mountain.add(new ArrayDeque<>());
+                mountain.get(idx).addLast(height);
                 N++;
             }else if(command == 300){ // 지진
-                dq.pollLast();
+                int mountainSeq = stack.get(N - 1);
+                mountain.get(mountainSeq).pollLast();
+                if(mountain.get(mountainSeq).isEmpty()){
+                    mountain.remove(mountainSeq);
+                }
+
+                stack.remove(N - 1);
                 N--;
             }else if(command == 400){ // 등산 시뮬레이션
-                /*
-                    산 랜덤 선택
-                    오른쪽 이동하면 100만점
-                    케이블카 타면 100만점
-                    더이상 이동 못하면 해당 산 높이만큼 점수 추가
-
-                    첫 명령과 마지막 명령을 제외한 모든 명령이 산의 추가라면 약 550000의 산 개수가 됨
-                    오른쪽 이동이 반드시 바로 옆 산으로 가는 것이 아님
-                    오른쪽에 있는 산 중에서 내 위치보다 높기만 하면 되는듯
-                */
                 int m_index = Integer.parseInt(stk.nextToken()) - 1;
 
-                List<int[]> list = new ArrayList<>();
-                TreeMap<Integer, Integer> tMap = new TreeMap<>();
-                List<Integer> mountain = new ArrayList<>(dq);
-
-                for(int i = 0; i < N; i++){
-                    if(i == 0) {
-                        list.add(new int[]{mountain.get(i), 0});
-                        tMap.put(mountain.get(i), 0);
-                        continue;
-                    }
-
-                    int height = mountain.get(i);
-
-                    // 내 앞에 온 높이 중에서 나보다 낮되, 가장 높은 점수 + 100만점 => 내 위치에서의 점수
-                    List<Integer> cloneList = new ArrayList<>(tMap.keySet());
-                    
-                    int idx = binarySearch(cloneList, height);
-
-                    if(idx == -1){
-                        list.add(new int[]{height, 0});
-
-                        if(!tMap.containsKey(height))
-                            tMap.put(height, 0);
-                    } else{
-                        SortedMap<Integer, Integer> sortedMap = tMap.subMap(tMap.firstKey(), cloneList.get(idx) + 1);
-                        TreeSet<Integer> tSet = new TreeSet(sortedMap.values());
-
-                        int p = tSet.last() + 1000000;
-
-                        list.add(new int[]{height, p});
-                        if(tMap.containsKey(height) && tMap.get(height) >= p) continue; 
-                        tMap.put(height, p);
-                    }
-                }
-
-                int max = 0;
-                for(int i = 0; i < N; i++){
-                    max = Math.max(max, list.get(i)[0] + list.get(i)[1]);
-                }
-
-                int point = list.get(m_index)[1] + 1000000 + max;
+                int point = (stack.get(m_index) + mountain.size()) * 1000000;
+                point += (mountain.get(mountain.size() - 1).peekFirst());
                 sb.append(point).append('\n');
             }
         }
@@ -89,15 +63,17 @@ public class Main {
         System.out.println(sb.toString());
     }
 
-    private static int binarySearch(List<Integer> sortedList, int height){
-        int idx = -1;
-        int left = 0, right = sortedList.size() - 1;
+    private static int binarySearch(int height){
+        int idx = mountain.size();
+        int left = 0, right = mountain.size() - 1;
         while(left <= right){
             int mid = (left + right)/2;
-            if(sortedList.get(mid) < height){
-                idx = mid;
+
+            int minItem = mountain.get(mid).peekLast();
+            if(minItem < height){ // 현재 스택보다 크므로, 더 큰 스택을 확인해야함
                 left = mid + 1;
             }else{
+                idx = mid;
                 right = mid - 1;
             }
         }
@@ -109,4 +85,10 @@ public class Main {
 
 
 
-// 57 47 79 70 22 92 7 3 11 94 2 47 8 86
+/*
+// 3 2 5 4 3
+index{0,0}
+mount[0]{3, 2}
+mount[2]{5}
+
+*/
