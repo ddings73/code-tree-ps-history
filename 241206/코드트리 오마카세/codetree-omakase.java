c14766cdfd8803a7a077982bff3fd251f025ae7d
ddings <ddings7303@gmail.com>
2024-12-06 15:55:24 +0900

    Title: 코드트리 오마카세, Time: 1039ms, Memory: 48MB, Status: Passed - CodeTree

--- 241206/코드트리 오마카세/codetree-omakase.java
+++ 241206/코드트리 오마카세/codetree-omakase.java
@@ -3,8 +3,10 @@ import java.io.*;
 
 public class Main {
     private static int L, Q;
-    private static Map<String, Queue<Sushi>> sushi_map = new HashMap<>();
+    private static Map<String, List<Sushi>> sushi_map = new HashMap<>();
     private static Map<String, int[]> people = new HashMap<>();
+
+    private static List<int[]> query = new ArrayList<>();
 
     private static class Sushi{
         int t, x;
@@ -25,81 +27,76 @@ public class Main {
 
         while(Q-- > 0){ // max 10만
             stk = new StringTokenizer(br.readLine());
-            int query = Integer.parseInt(stk.nextToken());
-            if(query == 100){
-                // t 시각에 x위치에 name 초밥 생성
-                int t = Integer.parseInt(stk.nextToken());
-                int x = Integer.parseInt(stk.nextToken());
-                String name = stk.nextToken();
+            int command = Integer.parseInt(stk.nextToken());
+            int t = -1, x = -1, n = -1;
+            String name = null;
+
+            if(command == 100){
+                t = Integer.parseInt(stk.nextToken());
+                x = Integer.parseInt(stk.nextToken());
+                name = stk.nextToken();
 
                 if(sushi_map.containsKey(name)){
-                    Queue<Sushi> q = sushi_map.get(name);
-                    q.add(new Sushi(t, x));
+                    List<Sushi> list = sushi_map.get(name);
+                    list.add(new Sushi(t, x));
                 }else{
-                    Queue<Sushi> q = new ArrayDeque<>();
-                    q.add(new Sushi(t, x));
-                    sushi_map.put(name, q);
+                    List<Sushi> list = new ArrayList<>();
+                    list.add(new Sushi(t, x));
+                    sushi_map.put(name, list);
                 }
-            }else if(query == 200){
-                // name인 사람이 t 시각에 x 위치에서 n개 초밥 제거 ( 자신의 name과 동일해야 함 )
-                int t = Integer.parseInt(stk.nextToken());
-                int x = Integer.parseInt(stk.nextToken());
-                String name = stk.nextToken();
-                int n = Integer.parseInt(stk.nextToken());
+            }else if(command == 200){
+                t = Integer.parseInt(stk.nextToken());
+                x = Integer.parseInt(stk.nextToken());
+                name = stk.nextToken();
+                n = Integer.parseInt(stk.nextToken());
                 
                 people.put(name, new int[]{x, n, t});
-            }else if(query == 300){
-                // t 시각의 사람 수와 초밥 수 출력
-                int t = Integer.parseInt(stk.nextToken());
+            }else if(command == 300){
+                t = Integer.parseInt(stk.nextToken());
+            }
 
-                Map<String, int[]> new_people = new HashMap<>();
-                for(String name : people.keySet()){
-                    int[] info = people.get(name);
-                    int people_pos = info[0];
-                    int people_eat = info[1];
-                    int people_entry = info[2];
+            query.add(new int[]{command, t, x, n});
+        }
 
-                    if(!sushi_map.containsKey(name)){
-                        new_people.put(name, new int[]{people_pos, people_eat, people_entry});
-                        continue;
-                    }
+        for(String name : people.keySet()){
+            int[] info = people.get(name);
+            int p_pos = info[0];
+            int p_entry = info[2];
 
-                    Queue<Sushi> q = sushi_map.get(name);
-                    Queue<Sushi> new_q = new ArrayDeque<>();
-                    while(!q.isEmpty()){
-                        Sushi s = q.poll();
-                        // 사람 입장 전에 나온 초밥이라면
-                        if(s.t < people_entry){
-                            s.x = (s.x + people_entry - s.t) % L;
-                            s.t = people_entry;
-                            if(s.x == people_pos){
-                                people_eat--;
-                                continue;
-                            }
-                        }
-
-                        int new_s_pos = (s.x + t - s.t);
-                        int p_dummy = people_pos < s.x ? people_pos + L : people_pos;
-                        if(s.x <= p_dummy && p_dummy <= new_s_pos) people_eat--;
-                        else{
-                            s.t = t;
-                            s.x = new_s_pos % L;
-                            new_q.add(s);
-                        }
-
-                    }
-
-                    if(!new_q.isEmpty()) sushi_map.put(name, new_q);
-                    else sushi_map.remove(name);
-
-                    if(people_eat > 0) new_people.put(name, new int[]{people_pos, people_eat, people_entry});
+            int p_remove_time = 0;
+            for(Sushi s : sushi_map.get(name)){
+                int s_remove_time = 0;
+                if(s.t < p_entry){
+                    s.x = (s.x + p_entry - s.t) % L;
+                    s_remove_time = p_entry + (p_pos - s.x + L) % L; 
+                }else{
+                    s_remove_time = s.t + (p_pos - s.x + L) % L;
                 }
 
-                people = new_people;
-                
-                int sum = 0;
-                for(Queue<Sushi> q : sushi_map.values()) sum += q.size();
-                sb.append(people.size()).append(" ").append(sum).append("\n");
+                p_remove_time = Math.max(p_remove_time, s_remove_time);
+
+                query.add(new int[]{101, s_remove_time, -1, -1});
+            }
+            query.add(new int[]{201, p_remove_time, -1, -1});
+        }
+
+        Collections.sort(query, (o1, o2) -> {
+            if(o1[1] == o2[1]) return Integer.compare(o1[0], o2[0]);
+            return Integer.compare(o1[1], o2[1]);
+        });
+        
+        int p_cnt = 0, s_cnt = 0;
+        for(int[] q : query){
+            if(q[0] == 100){
+                s_cnt++;
+            }else if(q[0] == 101){
+                s_cnt--;
+            }else if(q[0] == 200){
+                p_cnt++;
+            }else if(q[0] == 201){
+                p_cnt--;
+            }else if(q[0] == 300){
+                sb.append(p_cnt).append(" ").append(s_cnt).append("\n");
             }
         }
 
