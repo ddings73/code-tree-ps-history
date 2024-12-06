985a63a8f9fb8519193303342833e0caa899b870
ddings <ddings7303@gmail.com>
2024-12-06 13:06:42 +0900

    Title: 코드트리 오마카세, Time: 95ms, Memory: 9MB, Status: Runtime Error - CodeTree

--- 241206/코드트리 오마카세/codetree-omakase.java
+++ 241206/코드트리 오마카세/codetree-omakase.java
@@ -8,15 +8,14 @@ import java.io.*;
 public class Main {
     private static int L, Q;
     private static Queue<Sushi> sushi = new ArrayDeque<>();
+    private static Map<String, Queue<Sushi>> sushi_map = new HashMap<>();
     private static Map<String, int[]> people = new HashMap<>();
 
     private static class Sushi{
         int t, x;
-        String name;
-        Sushi(int t, int x, String name){
+        Sushi(int t, int x){
             this.t = t;
             this.x = x;
-            this.name = name;
         }
     }
 
@@ -31,8 +30,8 @@ public class Main {
 
         while(Q-- > 0){ // max 10만
             stk = new StringTokenizer(br.readLine());
-            int q = Integer.parseInt(stk.nextToken());
-            if(q == 100){
+            int query = Integer.parseInt(stk.nextToken());
+            if(query == 100){
                 // t 시각에 x위치에 name 초밥 생성
                 int t = Integer.parseInt(stk.nextToken());
                 int x = Integer.parseInt(stk.nextToken());
@@ -49,63 +48,74 @@ public class Main {
                     }
                 }
 
-                sushi.add(new Sushi(t, x, name));
-            }else if(q == 200){
+                if(sushi_map.containsKey(name)){
+                    sushi_map.get(name).add(new Sushi(t, x));
+                }else{
+                    Queue<Sushi> q = new ArrayDeque<>();
+                    q.add(new Sushi(t, x));
+                    sushi_map.put(name, q);
+                }
+            }else if(query == 200){
                 // name인 사람이 t 시각에 x 위치에서 n개 초밥 제거 ( 자신의 name과 동일해야 함 )
                 int t = Integer.parseInt(stk.nextToken());
                 int x = Integer.parseInt(stk.nextToken());
                 String name = stk.nextToken();
                 int n = Integer.parseInt(stk.nextToken());
 
-                int sushi_cnt = sushi.size();
+                Queue<Sushi> q = sushi_map.get(name);
+                int sushi_cnt = q.size();
                 while(sushi_cnt-- > 0){
-                    Sushi s = sushi.poll();
+                    Sushi s = q.poll();
 
                     int time = t - s.t;
                     int pos = (s.x + time) % L;
-                    if(!s.name.equals(name) || pos != x){
+                    if(pos != x){
                         s.t = t;
                         s.x = pos;
-                        sushi.add(s);
+                        q.add(s);
                     }else n--;
                 }
 
                 if(n == 0) continue;
                 people.put(name, new int[]{x, n});
-            }else if(q == 300){
+            }else if(query == 300){
                 // t 시각의 사람 수 와 초밥 수 출력
                 int t = Integer.parseInt(stk.nextToken());
 
-                int sushi_cnt = sushi.size();
-                while(sushi_cnt-- > 0){
-                    Sushi s = sushi.poll();
-                    if(!people.containsKey(s.name)){
-                        s.x = (s.x + (t - s.t)) % L;
-                        s.t = t;
-                        sushi.add(s);
-                        continue;
-                    }
+                for(String name : people.keySet()){
+                    int[] info = people.get(name);
 
-                    int[] info = people.get(s.name);
+                    if(!sushi_map.containsKey(name)) continue;
+                    
+                    Queue<Sushi> q = sushi_map.get(name);
+                    int sushi_cnt = q.size();
+                    while(sushi_cnt-- > 0){
+                        Sushi s = q.poll();
 
-                    int time = t - s.t;
-                    if(time >= L) info[1]--;
-                    else{
-                        int p_pos = info[0];
-                        if(p_pos < s.x) p_pos += L; 
-                        if(s.x <= p_pos && p_pos <= (s.x + time)) info[1]--;
+                        int time = t - s.t;
+                        if(time >= L) info[1]--;
                         else{
-                            s.t = t;
-                            s.x = (s.x + time) % L;
-                            sushi.add(s);
+                            int p_pos = info[0];
+                            if(p_pos < s.x) p_pos += L; 
+                            if(s.x <= p_pos && p_pos <= (s.x + time)) info[1]--;
+                            else{
+                                s.t = t;
+                                s.x = (s.x + time) % L;
+                                q.add(s);
+                            }
                         }
+
+
+                        if(info[1] == 0) people.remove(name);
                     }
-
-
-                    if(info[1] == 0) people.remove(s.name);
+                }
+                
+                int sushi_cnt = 0;
+                for(String name : sushi_map.keySet()){
+                    sushi_cnt += sushi_map.get(name).size();
                 }
 
-                sb.append(people.size()).append(" ").append(sushi.size()).append("\n");
+                sb.append(people.size()).append(" ").append(sushi_cnt).append("\n");
             }
         }
 
