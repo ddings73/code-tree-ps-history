2bf010dff5ce97f0621a6b40b3e922a30b27abc2
ddings <ddings7303@gmail.com>
2024-12-22 12:29:08 +0900

    Title: 나무박멸, Time: 184ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 241222/나무박멸/tree-kill-all.java
@@ -0,0 +1,153 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    private static int N, M, K, C;
+
+    private static int[][] map, toxic;
+    private static Queue<Tree> q = new ArrayDeque<>();
+    private static int[] dr = {-1, 1, 0, 0, -1, -1, 1, 1};
+    private static int[] dc = {0, 0, 1, -1, -1, 1, -1, 1};
+
+    private static class Tree{
+        int r, c;
+        Tree(int r, int c){
+            this.r = r;
+            this.c = c;
+        }
+
+        public int areaCheck(){
+            int sum = map[r][c];
+            for(int i = 4; i < 8; i++){
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+
+                int limit = K;
+                while(limit-- > 0 && 0 <= nr && nr < N && 0 <= nc && nc < N){
+                    if(map[nr][nc] >= 0) sum += map[nr][nc]; 
+                    if(map[nr][nc] == -1 || map[nr][nc] == 0) break;
+                    nr = nr + dr[i];
+                    nc = nc + dc[i];
+                }
+            }
+            return sum;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(stk.nextToken());
+        M = Integer.parseInt(stk.nextToken());
+        K = Integer.parseInt(stk.nextToken());
+        C = Integer.parseInt(stk.nextToken());
+
+        map = new int[N][N];
+        toxic = new int[N][N];
+        for(int i = 0; i < N; i++){
+            stk = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++){
+                map[i][j] = Integer.parseInt(stk.nextToken());
+                if(map[i][j] > 0){
+                    q.add(new Tree(i, j));
+                }
+            }
+        }
+
+        int kill_count = 0;
+        while(M-- > 0){
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+                    if(toxic[i][j] > 0) toxic[i][j]--;
+                }
+            }
+            Queue<Tree> sub = new ArrayDeque<>();
+            while(!q.isEmpty()){
+                Tree tree = q.poll();
+                if(toxic[tree.r][tree.c] > 0){
+                    map[tree.r][tree.c] = 0;
+                    continue;
+                }
+                sub.add(tree);
+            }
+            q = new ArrayDeque<>(sub);
+
+            // step1
+            for(Tree tree : q){
+                for(int i = 0; i < 4; i++){
+                    int nr = tree.r + dr[i];
+                    int nc = tree.c + dc[i];
+                    if(nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+                    if(map[nr][nc] <= 0) continue;
+                    map[tree.r][tree.c]++;
+                }
+            }
+
+            // step2
+            boolean[][] visit = new boolean[N][N];
+            int[][] newMap = new int[N][N];
+            sub = new ArrayDeque<>();
+            while(!q.isEmpty()){
+                Tree tree = q.poll();
+
+                Queue<Tree> tmpQ = new ArrayDeque<>();
+                int count = 0;
+                for(int i = 0; i < 4; i++){
+                    int nr = tree.r + dr[i];
+                    int nc = tree.c + dc[i];
+                    if(nr < 0 || nr >= N || nc < 0 || nc >= N) continue;
+                    if(map[nr][nc] != 0 || toxic[nr][nc] > 0) continue;
+                    count++;
+
+                    Tree newTree = new Tree(nr, nc);
+                    tmpQ.add(newTree);
+                    if(!visit[nr][nc]){
+                        visit[nr][nc] = true;
+                        sub.add(newTree);
+                    }
+                }
+
+                while(!tmpQ.isEmpty()){
+                    Tree newTree = tmpQ.poll();
+                    newMap[newTree.r][newTree.c] += (map[tree.r][tree.c] / count);
+                }
+
+                sub.add(tree);
+            }
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < N; j++){
+                    map[i][j] += newMap[i][j];
+                }
+            }
+            q = new ArrayDeque<>(sub);
+
+            // step3
+            int max_kill = 0;
+            int r = 0, c = 0;
+            for(Tree tree : q){
+                int kill = tree.areaCheck();
+                if(max_kill < kill){
+                    max_kill = kill;
+                    r = tree.r;
+                    c = tree.c;
+                }
+            }
+
+            kill_count += max_kill;
+            toxic[r][c] = C + 1;
+            for(int i = 4; i < 8; i++){
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+
+                int limit = K;
+                while(limit-- > 0 && 0 <= nr && nr < N && 0 <= nc && nc < N){
+                    toxic[nr][nc] = C + 1;
+                    if(map[nr][nc] == -1 || map[nr][nc] == 0) break;
+                    nr = nr + dr[i];
+                    nc = nc + dc[i];
+                }
+            }
+        }
+
+        System.out.println(kill_count);
+    }
+}
