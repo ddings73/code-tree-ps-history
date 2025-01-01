e9991009609a4d69a8c9e9a5244a4ba9c80e61df
ddings <ddings7303@gmail.com>
2025-01-01 15:58:48 +0900

    Add files via upload

---
+++ .github/workflows/ReadmeMerge.java
@@ -0,0 +1,57 @@
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.*;
+
+public class ReadmeMerge {
+    private static final String rootPath = "D:\\Desktop\\github actions test\\code-tree-ps-history";
+
+    private static final StringBuilder header = new StringBuilder()
+            .append("# \uD83D\uDCD6 학습하기\n")
+            .append("\n")
+            .append("# \uD83E\uDD47 실전 훈련\n")
+            .append("|기록분류|이름|티어|유형|상태|최근 제출 코드|\n")
+            .append("|---|---|---|---|---|---|\n");
+
+    private static final StringBuilder footer = new StringBuilder();
+
+    public static void main(String[] args) {
+        try(BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(rootPath + "\\README.md"), StandardCharsets.UTF_8))){
+            writer.write(header.toString());
+            Files.walk(Paths.get(rootPath))
+                    .filter(Files::isRegularFile)
+                    .filter(path -> "README.md".equalsIgnoreCase(path.getFileName().toString()))
+                    .filter(path -> !path.toAbsolutePath().toString().equals(Paths.get(rootPath).toAbsolutePath().toString()))
+                    .forEach(path -> {
+                        try(BufferedReader br = Files.newBufferedReader(path, StandardCharsets.UTF_8)){
+                            while(br.ready()){
+                                String line = br.readLine();
+                                if(line.contains("기록분류")){
+                                    br.readLine(); // 테이블 구분선 필터링
+                                    String str = br.readLine();
+                                    while(!"".equals(str)){
+                                        writer.write(URLDecoder.decode(str, StandardCharsets.UTF_8) + "\n");
+                                        str = br.readLine();
+                                    }
+
+                                    if(footer.isEmpty()){
+                                        while(br.ready()){
+                                            str = br.readLine();
+                                            footer.append(str).append("\n");
+                                        }
+                                    }
+                                }
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+
+            writer.write(footer.toString());
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+}
